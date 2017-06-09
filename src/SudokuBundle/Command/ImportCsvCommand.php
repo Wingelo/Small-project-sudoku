c2252aaa90b76e0af9ca07ce1ed5f56e73f7b02f
Cédric <cedric.gamrat@gmail.com>
2017-06-09 10:40:09 +0200

    Indentation de tous les fichiers

--- src/SudokuBundle/Command/ImportCsvCommand.php
+++ src/SudokuBundle/Command/ImportCsvCommand.php
@@ -9,13 +9,13 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Helper\ProgressBar;
 
 use SudokuBundle\Entity\Grille;
+
 /**
  * Created by PhpStorm.
  * User: wingel
  * Date: 03/06/17
  * Time: 21:38
  */
-
 class ImportCsvCommand extends ContainerAwareCommand
 {
     protected function configure()
@@ -24,10 +24,12 @@ class ImportCsvCommand extends ContainerAwareCommand
             ->setName('import:sudoku:csv')
             ->setDescription('Import Sudoku from CSV File');
     }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->import($input,$output);
+        $this->import($input, $output);
     }
+
     protected function import(InputInterface $input, OutputInterface $output)
     {
         //Parameter Command
@@ -40,7 +42,7 @@ class ImportCsvCommand extends ContainerAwareCommand
 
         ]);
 
-        $winSudoku = 0;
+
         //End Parameter Command
         $data = $this->get($input, $output);
 
@@ -55,25 +57,25 @@ class ImportCsvCommand extends ContainerAwareCommand
         $progress = new ProgressBar($output, $size);
         $progress->start();
 
-        foreach($data as $row){
+        foreach ($data as $row) {
             $rowSudoku = explode(',', $row);
 
             $grille = new Grille();
             $grille->setUser($rowSudoku[0]);
-            $grille->setLine1(implode(",", array_slice($rowSudoku, 1,9)));
-            $grille->setLine2(implode(",", array_slice($rowSudoku, 10,9)));
-            $grille->setLine3(implode(",", array_slice($rowSudoku, 19,9)));
-            $grille->setLine4(implode(",", array_slice($rowSudoku, 28,9)));
-            $grille->setLine5(implode(",", array_slice($rowSudoku, 37,9)));
-            $grille->setLine6(implode(",", array_slice($rowSudoku, 46,9)));
-            $grille->setLine7(implode(",", array_slice($rowSudoku, 55,9)));
-            $grille->setLine8(implode(",", array_slice($rowSudoku, 64,9)));
-            $grille->setLine9(implode(",", array_slice($rowSudoku, 73,9)));
+            $grille->setLine1(implode(",", array_slice($rowSudoku, 1, 9)));
+            $grille->setLine2(implode(",", array_slice($rowSudoku, 10, 9)));
+            $grille->setLine3(implode(",", array_slice($rowSudoku, 19, 9)));
+            $grille->setLine4(implode(",", array_slice($rowSudoku, 28, 9)));
+            $grille->setLine5(implode(",", array_slice($rowSudoku, 37, 9)));
+            $grille->setLine6(implode(",", array_slice($rowSudoku, 46, 9)));
+            $grille->setLine7(implode(",", array_slice($rowSudoku, 55, 9)));
+            $grille->setLine8(implode(",", array_slice($rowSudoku, 64, 9)));
+            $grille->setLine9(implode(",", array_slice($rowSudoku, 73, 9)));
 
 
             $em->persist($grille);
 
-            if(($i % $bashSize) === 0){
+            if (($i % $bashSize) === 0) {
                 $em->flush();
                 $em->clear();
 
@@ -92,7 +94,7 @@ class ImportCsvCommand extends ContainerAwareCommand
             '',
             '=======================',
             'End CSV import BDD',
-            'Number User :'. $size,
+            'Number User :' . $size,
 
 
         ]);
@@ -103,9 +105,8 @@ class ImportCsvCommand extends ContainerAwareCommand
     }
 
 
-
-
-    protected function get(InputInterface $input, OutputInterface $output){
+    protected function get(InputInterface $input, OutputInterface $output)
+    {
         $fileName = 'data/input.csv';
 
         $converter = $this->getContainer()->get('import.csvtoarray');
